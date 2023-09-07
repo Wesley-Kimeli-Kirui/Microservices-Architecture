2a4ae26853330ba232697a79b78089586edd58bb
Wesley254 <wesleykirui2021@gmail.com>
2023-09-07 07:05:43 +0300

    Cart Service

--- TheJitu_commerce_Coupons/Data/AppDbContext.cs
+++ TheJitu_commerce_Coupons/Data/AppDbContext.cs
@@ -3,14 +3,10 @@ using TheJitu_commerce_Coupons.Model;
 
 namespace TheJitu_commerce_Coupons.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
 
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
-        {
-            
-        }
-
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Coupon> Coupons { get; set; }
     }
